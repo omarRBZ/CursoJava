da53ec7c8c209817679d67811d0deda7118d639c
omarRBZ <usa.92slipk@gmail.com>
2017-08-21 10:11:31 -0500

    actualizacion de metodos limpia

--- tarea/Palindromo.java
+++ tarea/Palindromo.java
@@ -4,17 +4,16 @@ public class Palindromo{
     Scanner leer = new Scanner(System.in);
     String caracteres="~!@#$%^&*()_+-=-[]{}|;/.,<>?:1234567890";
     String  cadena=leer.nextLine();
+    cadena=cadena.toLowerCase();
     char[] aCaracteres = cadena.toCharArray();
     String nuevaCadena="";
     String alreves="";
-
     for (int i=0;i<=caracteres.length()-1 ;i++ ) {
       for (int j=0; j<=cadena.length()-1 ; j++ ) {
         if (caracteres.charAt(i)==cadena.charAt(j)) {
         aCaracteres[j]=' ';
         }
       }
-
   }
 nuevaCadena=nuevaCadena.replace(" ", "");
 for (int i = nuevaCadena.length()-1; i>=0 ;i-- ) {
@@ -25,4 +24,30 @@ for (int i = nuevaCadena.length()-1; i>=0 ;i-- ) {
     System.out.println("es un Palindromo");
 
   }
-}}
+  String palabra=limpia(cadena);
+  StringBuilder reverse = new StringBuilder(cadena);
+  reverse.reverse();
+  if (palabra.equals(reverse.toString())) {
+    System.out.println("si es un Palindromo");
+  }
+  else{
+    System.out.println("no es un Palindromo");
+  }
+}
+public static String limpia(String p){
+  StringBuilder sb = new StringBuilder();
+  for (int i=0;i<p.length() ;i++ ) {
+    char c=p.charAt(i);
+    if (c>='a' && c<='z') {
+      sb.append(c);
+
+    }
+
+  }
+  System.out.println(sb);
+  return sb.toString();
+}
+
+
+
+}
