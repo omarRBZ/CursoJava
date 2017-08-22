35adf67fc56ffcb80c9798132e9fbe78f0d3c11b
omarRBZ <usa.92slipk@gmail.com>
2017-08-22 11:50:29 -0500

    metodo para recorrer las columnas primero y filas despues

--- tarea/Carretera.java
+++ tarea/Carretera.java
@@ -7,6 +7,7 @@ public class Carretera{
     int contador=0;
     int segmentos =0;
     int [][] Carretera= new int [x][y];
+    int []tiras=new int [x+1];
 
     for (int i=0; i< Carretera.length ; i++ ) {
       for (int j=0; j<Carretera.length; j++ ) {
@@ -14,18 +15,30 @@ public class Carretera{
         Carretera[i][j]=numero;
 
       }
-
-
     }
-    System.out.println();
-    for (int i=0;i<Carretera.length ;i++ ) {
-      for (int j=0; j<Carretera.length;j++  ) {
-        System.out.print(Carretera[i][j]+" ");
+
+    for (int k=0;k<y ;k++ ) {
+      for (int i=0;i<x ;i++ ) {
+        if (Carretera[i][k]==0){
+          contador++;
+          if (i == x-1 || Carretera[i+1][k]==1) {
+            tiras[contador]++;
+            contador=0;
+
+          }
+        }
+
+
       }
-      System.out.println();
-
 
     }
+    for (int i=0 ;i<tiras.length ;i++ ) {
+      if (tiras[i]!=0){
+        System.out.println(i+" "+tiras[i]);
+      }
+
+    }
+
 
   }
 }
